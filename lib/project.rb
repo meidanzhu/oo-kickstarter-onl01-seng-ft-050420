d4e9a426077c447c43d23d7f94918219bfc0f8a5
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-19 19:36:26 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,16 +1,15 @@
-class Project 
+class Project
   attr_accessor :title, :backers
 
   def initialize
     @title = title
     @backers = []
-    
+
   end
 
   def add_backer(backer)
-    @backers << backer 
+    @backers << backer
     backer.project << self
-    
+
   end
 end
-    
