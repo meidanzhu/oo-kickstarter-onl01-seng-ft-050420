37ef10d3da8fe33a02ced9b4ebf08a5b186b12e2
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-19 19:31:59 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+  attr_reader :name, :backed_project
+
+    
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_project << project
+    project.backers << self
+  end
+
+
+
+end
